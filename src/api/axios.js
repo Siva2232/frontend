926b638744a36cb715efbcc43805897ca9448883
Siva2232 <sivadathanmb@gmail.com>
2026-02-19 12:59:14 +0530

    done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://webbackend-fsxr.onrender.com/api",
+  baseURL: "/api",
 });
 
 // Automatically attach token if available
